1a22f22fd92b9fb5782687d3ea577f15bfa28be2
Bruce Wang <taolinwant@gmail.com>
2019-03-25 21:54:56 +0800

    a part of chapter2

---
+++ chapter01/2_72.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <stdlib.h>
+
+void copy_int(int val, void* buf, int maxbytes)
+{
+	if (maxbytes - (int) sizeof(val) >= 0) {
+		memcpy(buf, (void*) &val, sizeof(val));
+	}
+}
+
+int main()
+{
+	int maxbytes = sizeof(int) * 10;
+	void* buf = malloc(maxbytes);
+	int val;
+
+	val = 0x12345678;
+	copy_int(val, buf, maxbytes);
+	assert(*(int*)buf == val);
+
+	val = 0xAABBCCDD;
+	copy_int(val, buf, 0);
+	assert(*(int*)buf == val);
+
+	free(buf);
+
+	return 0;
+}
